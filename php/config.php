03537643e76522a76005d4f546d058f9cb75bdbc
leo7044 <cc.ta.leo7044@gmail.com>
2017-11-21 09:30:03 +0100

    Datenbankanbindung geändert

--- php/config.php
+++ php/config.php
@@ -2,8 +2,8 @@
 /* Developer: Leo Brandenburg */
 error_reporting(0); // important to set
 $server = "localhost";
-$database = "reports";
-$username = "root";
-$password = "";
+$database = "stausauf";
+$username = "stausauf";
+$password = "stausauf";
 $conn = new mysqli($server, $username, $password, $database);
 ?>
